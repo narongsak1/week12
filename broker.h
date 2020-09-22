85ebf5da102e6f20bf378eb5038b1ecb851e851a
narongsak1 <69183697+narongsak1@users.noreply.github.com>
2020-09-22 11:46:02 +0700

    Add files via upload

---
+++ broker.h
@@ -0,0 +1,9 @@
+#ifndef BROKER_H_INCLUDED
+#define BROKER_H_INCLUDED
+
+int mtqq();
+int ifelse();
+int grade();
+int grade2();
+
+#endif // BROKER_H_INCLUDED
---
+++ grade.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int grade(){
+    int marks;
+    printf("Enter marks : ");
+    scanf("%d",&marks);
+    if(marks>=80 & marks<=100){
+        printf("A");
+    }
+    else if(marks>=70 & marks<=79){
+        printf("B");
+    }
+    else if(marks>=60 & marks<=69){
+        printf("C");
+    }
+    else if(marks>=50 & marks<=59){
+        printf("D");
+    }
+    else if(marks>=0 & marks<=50){
+        printf("F");
+    }
+
+
+}
